5e790ec53a5c0b8ce40ac6b1f1d392402669ce46
FMakareev <19980911Fender>
2018-07-03 23:34:38 +0300

    build and auto commit

--- src/config.js
+++ src/config.js
@@ -4,8 +4,8 @@ export const TABLE_NAME = 'places_list'; // имя таблицы
 
 export const HARD_CODE_MODE = false;
 
-export const DATA_URL = HARD_CODE_MODE ? 'assets/map.json' : 'http://1288987.lglwrk.web.hosting-test.net/map'; // адрес получения данных карты
-export const DB_VERSION_URL = 'http://1288987.lglwrk.web.hosting-test.net/updated_at'; // адрес получения версии БД
+export const DATA_URL = HARD_CODE_MODE ? 'assets/map.json' : 'http://185.25.117.8/map'; // адрес получения данных карты
+export const DB_VERSION_URL = 'http://185.25.117.8/updated_at'; // адрес получения версии БД
 export const DROP_TABLE = false; // удолять таблицу перед создание
 
 
