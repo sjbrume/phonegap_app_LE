7d09fd7f0106c3dc85dabf3a91038b6bb75a1826
fmakareev <fmakareev@code-artel.com>
2019-10-30 13:19:40 +0300

    format

--- src/routes/complaints/index.js
+++ src/routes/complaints/index.js
@@ -7,161 +7,180 @@ import {TABLE_NAME} from "../../config";
 
 
 const lexicon = {
-    'RU': {
-        load_map: ' Загрузка карты...'
+	'RU': {
+		load_map: ' Загрузка карты...'
 
-    },
-    'UKR': {
-        load_map: 'Завантаження карти ...'
-
-    }
-}
+	},
+	'UKR': {
+		load_map: 'Завантаження карти ...'
+	}
+};
 
 function mapStateToProps(state) {
-    return {
-        currentLocal: state.intl,
-        db: {
-            db: state.websql.db.db,
-            loading: state.websql.db.loading,
-            error: state.websql.db.error,
-            success: state.websql.db.success,
-        },
-        data: {
-            loading: state.websql.data.loading,
-            error: state.websql.data.error,
-            success: state.websql.data.success,
-        },
-        set: {
-            loading: state.websql.set.loading,
-            error: state.websql.set.error,
-            success: state.websql.set.success,
-        },
-        version: {
-            version: state.websql.version.version,
-            loading: state.websql.version.loading,
-            error: state.websql.version.error,
-            success: state.websql.version.success,
-        }
-    }
+	return {
+		currentLocal: state.intl,
+		db: {
+			db: state.websql.db.db,
+			loading: state.websql.db.loading,
+			error: state.websql.db.error,
+			success: state.websql.db.success,
+		},
+		data: {
+			loading: state.websql.data.loading,
+			error: state.websql.data.error,
+			success: state.websql.data.success,
+		},
+		set: {
+			loading: state.websql.set.loading,
+			error: state.websql.set.error,
+			success: state.websql.set.success,
+		},
+		version: {
+			version: state.websql.version.version,
+			loading: state.websql.version.loading,
+			error: state.websql.version.error,
+			success: state.websql.version.success,
+		}
+	}
 }
 
 function mapDispatchToProps(dispatch) {
-    return {
-        dispatch: (type, payload) => {
-            dispatch({type, payload})
-        }
-    }
+	return {
+		dispatch: (type, payload) => {
+			dispatch({type, payload})
+		}
+	}
 }
 
 @connect(mapStateToProps, mapDispatchToProps)
 export class ComplaintsPage extends Component {
 
-    static propTypes = {};
+	static propTypes = {};
 
-    static defaultProps = {};
+	static defaultProps = {};
 
-    constructor(props) {
-        super(props);
-        this.state = this.initialState;
-        this.searchCompany = this.searchCompany.bind(this);
-    }
+	constructor(props) {
+		super(props);
+		this.state = this.initialState;
+		this.searchCompany = this.searchCompany.bind(this);
+	}
 
-    get initialState() {
-        return {
-            loading: true,
-            company: null
-        }
-    }
+	get initialState() {
+		return {
+			loading: true,
+			company: null,
+			isSearchCompany: false,
+		}
+	}
 
-    async componentWillReceiveProps(nextProps) {
-        const {db, set,} = nextProps;
-        if (db !== this.props.db.db && set.success) {
-            console.log(this.props);
-            const data = await this.searchCompany();
-            console.log(data);
-        }
-    }
+	// async componentWillReceiveProps(nextProps) {
+	//     const {db, set,} = nextProps;
+	//     if (db !== this.props.db.db && set.success && !this.state.loading && !this.state.loading.company) {
+	//         console.log('componentWillReceiveProps: ',nextProps);
+	//         console.log('componentWillReceiveProps: ',this.props);
+	//         const data = await this.searchCompany();
+	//         console.log(data);
+	//     }
+	// }
 
-    async componentWillMount() {
-        if (this.props.match.params.id) {
-            this.setState({loading: true});
-            const data = await this.searchCompany();
-            console.log(data);
-        } else {
-            this.setState({loading: false});
-        }
-    }
+	async componentWillMount() {
+		console.log('componentWillMount');
+		if (this.props.match.params.id) {
 
-    searchCompany() {
-        const {db} = this.props;
-        return new Promise((resolve, reject) => {
-            if (this.props.match.params.id) {
-                try {
-                    ('db' in db) && this.props.db.db.transaction((tx) => {
-                        tx.executeSql(`SELECT *
-                                       FROM ${TABLE_NAME}
-                                       WHERE id = ?;`,
-                            [this.props.match.params.id],
-                            (sqlTransaction, sqlResultSet) => {
-                                console.log(sqlResultSet.rows.item);
-                                if (sqlResultSet.rows.item) {
-                                    this.setState({company: sqlResultSet.rows.item(0), loading: false});
-                                    resolve(sqlResultSet.rows.item(0))
-                                } else {
+			const data = await this.searchCompany();
+			console.log('data: ', data);
+		} else {
+			this.setState({loading: false});
+		}
+	}
 
-                                    resolve(false)
-                                }
-                            }, (sqlTransaction, sqlEerror) => {
-                                console.log(sqlTransaction, sqlEerror);
-                            })
-                    })
-                } catch (err) {
-                    console.log(err);
-                }
-            } else {
-                this.setState({loading: false});
+	searchCompany() {
+		const {db} = this.props;
 
-                resolve(true);
-            }
-        }).then((res) => res)
-    }
+		this.setState({loading: true});
+		return new Promise((resolve, reject) => {
+			console.log('searchCompany', this.props);
+			if (this.props.match.params.id) {
 
-    render() {
-        const {currentLocal} = this.props;
-        console.log(this);
-        return (
-            <div className="complaints_wrapper">
-                {
-                    this.state.loading && <div className="loading-panel_wrapper">
-                        <div>
-                            <CircularProgress style={{
-                                display: 'block',
-                                color: '#0277bd',
-                                margin: '0 auto'
-                            }} size={60} thickness={7}/>
-                            <div className="loading-panel_content">
-                                {lexicon[currentLocal].load_map}
-                            </div>
-                        </div>
-                    </div>
-                }
-                {
-                    !this.state.loading &&
-                    <FormComplaints params={this.props.match.params} initialValues={{
-                        ...(this.state.company && this.state.company.company ? {
-                            company: this.state.company && this.state.company.company ? this.state.company.company : '',
 
-                        } : null),
-                        ...(this.state.company && this.state.company.id ? {
-                            address_id: this.state.company && this.state.company.id ? this.state.company.id : '',
-                        } : null),
-                        ...(this.state.company && this.state.company.address ? {
-                            address: this.state.company && this.state.company.address ? this.state.company.address : '',
-                        } : null),
+				console.log('setInterval: ', db);
+				if (('db' in db) && ('transaction' in db.db)) {
 
-                    }}/>
-                }
-            </div>
-        )
-    }
+					const executeSql = `SELECT * FROM ${TABLE_NAME} WHERE id = ?;`;
+					db.db.transaction((tx) => {
+						tx.executeSql(executeSql,
+							[this.props.match.params.id],
+							(sqlTransaction, sqlResultSet) => {
+								console.log(sqlResultSet.rows.item);
+								if (sqlResultSet.rows.item) {
+									this.setState({company: sqlResultSet.rows.item(0), loading: false});
+									resolve(sqlResultSet.rows.item(0))
+								} else {
+									this.setState({loading: false});
+
+									resolve(false)
+								}
+							}, (sqlTransaction, sqlEerror) => {
+								console.log(sqlTransaction, sqlEerror);
+							})
+					});
+				}
+
+
+				try {
+					// ('db' in db)
+					// && ('transaction' in db.db)
+
+				} catch (err) {
+					console.log(err);
+					this.setState({loading: false});
+				}
+			} else {
+				this.setState({loading: false});
+
+				resolve(true);
+			}
+		}).then((res) => res)
+	}
+
+	render() {
+		const {currentLocal} = this.props;
+		console.log(this);
+		return (
+			<div className="complaints_wrapper">
+				{
+					this.state.loading && <div className="loading-panel_wrapper">
+						<div>
+							<CircularProgress style={{
+								display: 'block',
+								color: '#0277bd',
+								margin: '0 auto'
+							}} size={60} thickness={7}/>
+							<div className="loading-panel_content">
+								{lexicon[currentLocal].load_map}
+							</div>
+						</div>
+					</div>
+				}
+				{
+					!this.state.loading &&
+					<FormComplaints
+						params={this.props.match.params}
+						initialValues={{
+							...(this.state.company && this.state.company.company ? {
+								company: this.state.company && this.state.company.company ? this.state.company.company : '',
+
+							} : null),
+							...(this.state.company && this.state.company.id ? {
+								address_id: this.state.company && this.state.company.id ? this.state.company.id : '',
+							} : null),
+							...(this.state.company && this.state.company.address ? {
+								address: this.state.company && this.state.company.address ? this.state.company.address : '',
+							} : null),
+
+						}}/>
+				}
+			</div>
+		)
+	}
 }
